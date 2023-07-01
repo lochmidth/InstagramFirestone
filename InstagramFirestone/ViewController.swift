7734f450f67beea8a7f5bef62ef88b73aa0ab521
lochmidth <alphanogun@gmail.com>
2023-07-01 14:52:53 +0300

    Initial Commit

---
+++ InstagramFirestone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestone
+//
+//  Created by Alphan Ogün on 1.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
