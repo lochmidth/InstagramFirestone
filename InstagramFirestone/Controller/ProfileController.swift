615c23608086e6dd46720b95d0e36642ff90f8b3
lochmidth <alphanogun@gmail.com>
2023-08-17 16:28:02 +0300

    Complete User Feed with Followers
    Now User feed will show posts from current user's following acc.

--- InstagramFirestone/Controller/ProfileController.swift
+++ InstagramFirestone/Controller/ProfileController.swift
@@ -146,6 +146,8 @@ extension ProfileController: ProfileHeaderDelegate {
             UserService.unfollow(uid: user.uid) { error in
                 self.user.isFollowed = false
                 self.collectionView.reloadData()
+                
+                PostService.updateUserFeedAfterFollowing(user: user, didFollow: false)
             }
         } else {
             UserService.follow(uid: user.uid) { error in
@@ -155,6 +157,8 @@ extension ProfileController: ProfileHeaderDelegate {
                 NotificationService.uploadNotification(toUid: user.uid,
                                                        fromUser: currentUser,
                                                        type: .follow)
+                
+                PostService.updateUserFeedAfterFollowing(user: user, didFollow: true)
             }
         }
     }
